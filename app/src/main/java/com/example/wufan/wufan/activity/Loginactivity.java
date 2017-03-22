ad996b0d8a8f8688b8e3602d50bf8fdeca2f41b6
zhaoxu-me <larry@LarrydeMacBook-Pro.local>
2017-03-22 10:01:33 +0800

    wufan

---
+++ app/src/main/java/com/example/wufan/wufan/activity/Loginactivity.java
@@ -0,0 +1,21 @@
+package com.example.wufan.wufan.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wufan.wufan.R;
+
+public class Loginactivity extends AppCompatActivity {
+
+    String customerKey = "a1f21489af5f73e1d21f2ee77574282e" ;
+    String consumerSecret = "c67f124590f52f23bde2e63853c0e2b3";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginactivity);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/wufan/wufan/util/NetUtils.java
@@ -0,0 +1,10 @@
+package com.example.wufan.wufan.util;
+
+/**
+ * Created by chenlongfei on 2016/12/21.
+ */
+
+public final class NetUtils {
+    private NetUtils() {
+    }
+}
