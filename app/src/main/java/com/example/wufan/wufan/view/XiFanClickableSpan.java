ad9648b9f4e3c3780662a23130d367e034dd6649
zhaoxu-me <larry@LarrydeMacBook-Pro.local>
2017-03-26 00:00:12 +0800

    2017-3-25
    登陆接口可以开始调用。

---
+++ app/src/main/java/com/example/wufan/wufan/view/XiFanClickableSpan.java
@@ -0,0 +1,38 @@
+package com.example.wufan.wufan.view;
+
+import android.os.Bundle;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
+import android.view.View;
+import com.chenenyu.router.Router;
+import com.orhanobut.logger.Logger;
+import com.example.wufan.wufan.R;
+
+/**
+ * Created by chenlongfei on 2016/12/15.
+ */
+
+public class XiFanClickableSpan extends ClickableSpan {
+    private URLSpan urlSpan;
+
+    public XiFanClickableSpan(URLSpan urlSpan) {
+        this.urlSpan = urlSpan;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Logger.d("URL:" + urlSpan.getURL());
+        Bundle bundle = new Bundle();
+        //bundle.putString(BrowserActivity.BUNDLE_URL, "http://www.baidu.com");
+        //Router.build(Constants.Router.BROWSER).extras(bundle).go(view.getContext());
+        Router.build(urlSpan.getURL()).go(view.getContext());
+    }
+
+    @Override
+    public void updateDrawState(TextPaint ds) {
+        super.updateDrawState(ds);
+        //ds.setColor(App.getInstance().getResources().getColor(R.color.colorPrimary));
+        ds.setUnderlineText(false);
+    }
+}
